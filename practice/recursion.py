5d30fdf21806434a9dc696a980b82d4cd3106e27
imtiaz <imtiazya@protonmail.com>
2026-05-13 14:01:21 +0100

    updated practice/recursion

--- practice/recursion.py
+++ practice/recursion.py
@@ -20,21 +20,29 @@
 
 # example of base and recursive case
 
-# def factorial(n):
+def factorial(n):
     # base case
-#    if n == 0 or n == 1:
-#        return 1
+    if n == 0 or n == 1:
+        return 1
     # recursive case
-#    else:
-#        return n * factorial(n - 1)
+    else:
+        return n * factorial(n - 1)
 
-# print(factorial(5))
+print(factorial(5))
 
+# calculating the sum all of numbers up to num
+def sum_recursive(num):
+    if num == 1: # base case
+        return num
+    return num + sum_recursive(num - 1) # recursive case 
 
-# def inception(dreams):
-#    if dreams == 0:
-#        return 
-#    inception(dreams - 1)
-#    print(f"this is dream {dreams} within a dream")
+print(sum_recursive(3))
 
-# inception(100)
+# calculating the power of a number 
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    else: 
+        return base * power(base, exponent - 1)
+    
+print(power(2, 4))
